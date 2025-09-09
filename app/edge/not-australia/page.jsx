366a87be5486ea9244de21b18a626ee99321a65f
calebm01 <calebrmouritsen@gmail.com>
2025-09-09 13:38:12 -0600

    Fixed WPF page

--- app/edge/not-australia/page.jsx
+++ app/edge/not-australia/page.jsx
@@ -11,37 +11,41 @@ export default function FallbackPage() {
         <>
             <h1 className="mb-8">Airline Seat Reservation System</h1>
             <section>
-                <h2 className="mb-6">This assignment was the last solo venture we were tasked with in my WPF class at Weber State University.
+                <h3>This assignment was the last solo venture we were tasked with in my WPF class at Weber State University.
                     This project entailed creating a system for users to view 2 plane seating arrangements.
-                    Users can click on seats to occupy them, and also have the option to change seating, or remove their seat entirely.</h2>
+                    Users can click on seats to occupy them, and also have the option to change seating, or remove their seat entirely.</h3>
                 <figure>
-                    <div className="relative overflow-hidden border-2 border-white rounded-lg aspect-3/2">
+                    <div className="inline-block overflow-hidden border-2 border-white rounded-lg">
                         <Image
                             src="/images/AirlineReservation.png"
-                            priority
-                            fill={true}
                             sizes="(max-width: 1024px) 100vw, 1024px"
                             alt="Corgi"
                         />
                     </div>
                 </figure>
+                
+                <figure>
+                    <div className="inline-block overflow-hidden border-2 border-white rounded-lg">
+                        <Image
+                            src="/images/AirlineReservation2.png"
+                            sizes="(max-width: 1024px) 100vw, 1024px"
+                            alt="Corgi"
+                        />
+                    </div>
+                </figure>              
+            </section>
+            <Link href="https://github.com/calebm01/Airline-Reservation-System">
+                    Repository Link
+                </Link>
 
-                <div className="relative overflow-hidden border-2 border-white rounded-lg aspect-3/2">
-                    <Image
-                        src="/images/AirlineReservation2.png"
-                        priority
-                        fill={true}
-                        sizes="(max-width: 1024px) 100vw, 1024px"
-                        alt="Corgi"
-                    />
-                </div>
+            <section>
+            <h1 className="mb-8">Invoice System</h1>
             </section>
 
-            <h1 className="mb-8">Invoice System</h1>
             <section>
-                <h2 className="mb-6">The final project for my course at weber state covering WPF applications.
+                <h3>The final project for my course at weber state covering WPF applications.
                     Me and my group were tasked to create an invoice system suitable for a business to update and manage a sizeable inventory.
-                    In particular in this project, I worked on the Item Window which allowed administration to add, edit, and delete items within the inventory.</h2>
+                    In particular in this project, I worked on the Item Window which allowed administration to add, edit, and delete items within the inventory.</h3>
 
                 <Link href="https://github.com/cameron-mckay/InvoiceSystem">
                     Repository Link
--- app/edge/page.jsx
+++ app/edge/page.jsx
@@ -11,9 +11,9 @@ export default function Page() {
         <>
             <h1 className="mb-8">Airline Seat Reservation System</h1>
             <section>
-                <h4>This assignment was the last solo venture we were tasked with in my WPF class at Weber State University.
+                <h3>This assignment was the last solo venture we were tasked with in my WPF class at Weber State University.
                     This project entailed creating a system for users to view 2 plane seating arrangements.
-                    Users can click on seats to occupy them, and also have the option to change seating, or remove their seat entirely.</h4>
+                    Users can click on seats to occupy them, and also have the option to change seating, or remove their seat entirely.</h3>
                 <figure>
                     <div className="inline-block overflow-hidden border-2 border-white rounded-lg">
                         <Image
@@ -43,9 +43,9 @@ export default function Page() {
             </section>
 
             <section>
-                <h4>The final project for my course at weber state covering WPF applications.
+                <h3>The final project for my course at weber state covering WPF applications.
                     Me and my group were tasked to create an invoice system suitable for a business to update and manage a sizeable inventory.
-                    In particular in this project, I worked on the Item Window which allowed administration to add, edit, and delete items within the inventory.</h4>
+                    In particular in this project, I worked on the Item Window which allowed administration to add, edit, and delete items within the inventory.</h3>
 
                 <Link href="https://github.com/cameron-mckay/InvoiceSystem">
                     Repository Link
